0632e1e3046ceb31dd39759d17e7628ca9409c16
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-08 15:49:41 +0530

    Opening and Closing the SideMenu with Animation

--- SideMenu Animation/ViewController.swift
+++ SideMenu Animation/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var showButton: UIButton!
+    @IBOutlet var hideButton: UIButton!
+    @IBOutlet var sideMenuView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        hideMenu()
     }
 
+    func hideMenu() {
+        UIView.animate(withDuration: 0.4, animations: {
+               // self.view.alpha = 0
+        })
+        
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseOut, animations: {
+            self.sideMenuView.transform = CGAffineTransform(translationX: -self.sideMenuView.frame.width-20, y: 0)
+        })
+        
+    }
+    
+    func showMenu() {
+        UIView.animate(withDuration: 0.4, animations: {
+            //  self.view.alpha = 0
+        })
+        
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseOut, animations: {
+            self.sideMenuView.transform = .identity
+        })
+    }
 
+    @IBAction func showMenu(_ sender: Any) {
+        
+        showMenu()
+        
+    }
+    
+    @IBAction func hideMenu(_ sender: Any) {
+        hideMenu()
+    }
+    
 }
 
